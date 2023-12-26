f5845cc3d169ea06325023560cd7756ddc0f3b50
Jonah TzCh <jonah_mytzuchi@outlook.com>
2023-12-26 10:43:36 +0800

    update tool's description

--- bots/TeleAssistantBot/src/gpt/tools.py
+++ bots/TeleAssistantBot/src/gpt/tools.py
@@ -8,47 +8,105 @@ from duckduckgo_search import DDGS
 class GoogleSearchAgent:
     def __init__(self):
         self.__name = "GoogleSearchAgent"
-        self.__google_se = GoogleSearchAPIWrapper() # configurations are done Google Site
+        self.__google_se = (
+            GoogleSearchAPIWrapper()
+        )  # configurations are done Google Site
 
     def description(self):
-        pass
+        return {
+            "name": "GoogleSearch",
+            "description": "Search the web through Google Search Engine, utilize this function when recent knowledge or information is needed.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "query": {
+                        "type": "string",
+                        "description": "Keywords used to run the search",
+                    },
+                    "max_results": {
+                        "type": "integer",
+                        "description": "The number of results return from the search",
+                        "default": 5,
+                        "validation": {
+                            "min_value": 1,
+                            "max_value": 10,
+                            "error_message": "max_results must be an integer between 1 and 10",
+                        },
+                    },
+                },
+                "required": ["query"],
+            },
+        }
 
     def __call__(self, params: object):
         query = params.get("query")
         max_results = params.get("max_results")
         max_results = 5 if max_results is None else int(max_results)
-        
+
         print(f"Begin Execution: {self.__name}...")
         output = dict()
-        output['result'] = self.__google_se.results(query, max_results)
+        output["result"] = self.__google_se.results(query, max_results)
         print(f"End Execution: {self.__name}")
         return json.dumps(output)
+
 
 class DuckDuckGoSearchAgent:
     def __init__(self):
         self.__name = "DuckDuckGoSearchAgent"
-        self.__safesearch = "moderate" # on, moderate, off
+        self.__safesearch = "moderate"  # on, moderate, off
         self.__region = "my-en"
 
     def description(self):
-        pass
-        
+        return {
+            "name": "DuckDuckGoSearch",
+            "description": "Search the web through DuckDuckGo Search Engine, utilize this function when recent knowledge or information is needed.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "query": {
+                        "type": "string",
+                        "description": "Keywords used to run the search",
+                    },
+                    "max_results": {
+                        "type": "integer",
+                        "description": "The number of results return from the search",
+                        "default": 5,
+                        "validation": {
+                            "min_value": 1,
+                            "max_value": 10,
+                            "error_message": "max_results must be an integer between 1 and 10",
+                        },
+                    },
+                },
+                "required": ["query"],
+            },
+        }
+
     def __call__(self, params: object):
         query = params.get("query")
         max_results = params.get("max_results")
         max_results = 5 if max_results is None else int(max_results)
-            
+
         print(f"Begin Execution: {self.__name}...")
         output = dict()
         with DDGS() as ddgs:
-            output['result'] = [r for r in ddgs.text(keywords=query, region=self.__region, safesearch = self.__safesearch, max_results=max_results)]
+            output["result"] = [
+                r
+                for r in ddgs.text(
+                    keywords=query,
+                    region=self.__region,
+                    safesearch=self.__safesearch,
+                    max_results=max_results,
+                )
+            ]
         print(f"End Execution: {self.__name}")
         return json.dumps(output)
-    
-    
+
+
 function_dictionary = dict()
-function_dictionary['GoogleSearch'] = GoogleSearchAgent()
-function_dictionary['DuckDuckGoSearch'] = DuckDuckGoSearchAgent()
+function_dictionary["GoogleSearch"] = GoogleSearchAgent()
+function_dictionary["DuckDuckGoSearch"] = DuckDuckGoSearchAgent()
+
 
 def execute_function(function_name: str, arguements: str):
     if not function_name in function_dictionary.keys():
