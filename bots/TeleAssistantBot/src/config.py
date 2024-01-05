c7e4802bd087e1575229508f1fc3d70ef4da1482
Jonah TzCh <jonah_mytzuchi@outlook.com>
2024-01-05 10:34:11 +0800

    move assistant objective to assistant.yml page

--- bots/TeleAssistantBot/src/config.py
+++ bots/TeleAssistantBot/src/config.py
@@ -10,33 +10,33 @@ config_env = dotenv.dotenv_values(config_dir / "config.env")
 mongodb_uri = f"mongodb://{config_env['MONGODB_HOST']}:{config_env['MONGODB_PORT']}"
 
 # load yaml config
-with open(config_dir / "config.yml", 'r') as f:
+with open(config_dir / "config.yml", "r") as f:
     config_yaml = yaml.safe_load(f)
 
-telegram_token = config_yaml['telegram_token']
-openai_api_key = config_yaml['openai_api_key']
+telegram_token = config_yaml["telegram_token"]
+openai_api_key = config_yaml["openai_api_key"]
 
-os.environ["GOOGLE_CSE_ID"] = config_yaml['GOOGLE_CSE_ID']
-os.environ['GOOGLE_API_KEY'] = config_yaml['GOOGLE_API_KEY']
+os.environ["GOOGLE_CSE_ID"] = config_yaml["GOOGLE_CSE_ID"]
+os.environ["GOOGLE_API_KEY"] = config_yaml["GOOGLE_API_KEY"]
 
-allowed_user_ids = config_yaml['allowed_user_ids']
-system_admin_ids = config_yaml['system_admin_ids']
+allowed_user_ids = config_yaml["allowed_user_ids"]
+system_admin_ids = config_yaml["system_admin_ids"]
 
-max_interactions = config_yaml['max_interactions']
-max_file_count_per_thread = config_yaml['max_file_count_per_thread']
-idle_timeout = config_yaml['idle_timeout']
-temperature = config_yaml['temperature']
-max_retries = config_yaml['max_retries']
+max_interactions = config_yaml["max_interactions"]
+max_file_count_per_thread = config_yaml["max_file_count_per_thread"]
+idle_timeout = config_yaml["idle_timeout"]
+temperature = config_yaml["temperature"]
+max_retries = config_yaml["max_retries"]
 
 # load assistant.yml
-with open(config_dir / "assistant.yml", 'r') as f:
+with open(config_dir / "assistant.yml", "r") as f:
     assistant_yaml = yaml.safe_load(f)
-    
-assistant = assistant_yaml['assistant']
-default_assistant_name = "Nurse Manager assistant" # list(assistant.keys())[-1] # nurse manager assistant
 
-wikipedia_user_agent = config_yaml['wikipedia_user_agent']
-weatherapi_api_key = config_yaml['weatherapi_api_key']
+wikipedia_user_agent = config_yaml["wikipedia_user_agent"]
+weatherapi_api_key = config_yaml["weatherapi_api_key"]
 
-assistant_objective = assistant_yaml['objective']
-l2_moderator_prompt = config_yaml['l2_moderator_prompt']
+assistant = assistant_yaml["assistant"]
+default_assistant_name = "General Data Analyst"
+
+assistant_objective = assistant[default_assistant_name]["objective"]
+l2_moderator_prompt = config_yaml["l2_moderator_prompt"]
